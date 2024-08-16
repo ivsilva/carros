672db24bd5250da547821d7ed44cddeaf2befb3d
ivsilva <ivosilvasoares690@gmail.com>
2024-08-16 14:43:12 -0300

    geral

--- cars/admin.py
+++ cars/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
-from cars.models import Car, Brand  #Da pasta cars do arquivo models importar cars
+from cars.models import Car, Brand, CarImage  # Importar CarImage
 
-
+# Admin para Brand
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('model',)
-    search_fields = ['model',]
+    search_fields = ['model']
+
+# Admin para CarImage (opcional, se você deseja gerenciar imagens diretamente pelo admin)
+class CarImageAdmin(admin.ModelAdmin):
+    list_display = ('car', 'image')
+    search_fields = ['car__model']  # Permite buscar imagens por modelo do carro
+
+# Admin para Car com inline para CarImage
+class CarImageInline(admin.TabularInline):
+    model = CarImage
+    extra = 1  # Número de formulários em branco a serem exibidos inicialmente
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model','brand','factory_year','model_year','value')
-    search_fields = ['model',] #Defina quais nomes quer buscar
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ['model']
+    inlines = [CarImageInline]  # Adiciona o inline para imagens no admin de Car
 
+# Registro dos modelos no admin
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
+admin.site.register(CarImage, CarImageAdmin)  # Opcional, se você deseja acessar CarImage diretamente
--- cars/forms.py
+++ cars/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from cars.models import Car
+from cars.models import Car, CarImage
 
 class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+class CarImageForm(forms.ModelForm):
+    class Meta:
+        model = CarImage
+        fields = ['image']  # Somente o campo de imagem
